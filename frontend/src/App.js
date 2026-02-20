5aa4ed19fcc9d14cdf96ce2a27ea5e9422affa7a
Sahan2O4 <sahandesilva484@gmail.com>
2026-02-20 18:16:42 +0530

    Add polished Homepage with hero, why us section, image cards, and Google Map

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Homepage from "./pages/homepage"; // note lowercase if your file is homepage.js
+
+function App() {
+  return <Homepage />;
+}
+
+export default App;
